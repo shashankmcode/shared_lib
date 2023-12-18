f90d411468a4524288736c9b359e66c19173ffd7
Shashank <shashankhs5242@gmail.com>
2023-12-19 00:10:24 +0530

    map

--- vars/imagebuild.groovy
+++ vars/imagebuild.groovy
@@ -1,6 +1,6 @@
 def call() {
     sh """
         docker build -t shashank-java-app .
-        docker run -p 8080:8080 shashank-java-app
+        docker run -p 8081:8081 shashank-java-app
     """
 }
